b338bcc6f80b190f7f8eb2f4219944955d73273b
memo012 <1158821459@qq.com>
2020-12-13 22:08:25 +0800

    --other

--- resk/example/gin_server.go
+++ resk/example/gin_server.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	app := gin.Default()
-	app.GET("/hello", func(context *gin.Context) {
-		context.String(http.StatusOK, "hello gin")
-	})
-	app.Run()
+	//app.GET("/hello", func(context *gin.Context) {
+	//	context.String(http.StatusOK, "hello gin")
+	//})
+	//app.GET("/user/:name", func(context *gin.Context) {
+	//	name := context.Param("name")
+	//	context.String(http.StatusOK, name)
+	//})
+	app.Run(":8086")
 }
